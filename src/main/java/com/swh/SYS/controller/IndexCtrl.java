95dc96e8fcc80c848cb00de0ef87f42792fd3391
SWH <lane_swh@hotmail.com>
2017-11-09 09:31:13 +0800

    vue+element

--- src/main/java/com/swh/SYS/controller/IndexCtrl.java
+++ src/main/java/com/swh/SYS/controller/IndexCtrl.java
@@ -3,11 +3,15 @@ package com.swh.SYS.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigDecimal;
+
 @Controller
 public class IndexCtrl {
 
     @RequestMapping("/")
     public String index(){
+
+
         return "index";
     }
 }
